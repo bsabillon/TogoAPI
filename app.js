a86cce5bcfab86ae7be4e3447cd5dc6db933f183
bsabillon <38368514+bsabillon@users.noreply.github.com>
2019-07-30 13:56:04 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+var cors = require('cors');
+var pgp = require("pg-promise")();
+var database = pgp("postgres://ofsoasjazfznro:8422453c66450cd02fe7a41a3fa1d20882e038ab1345b76bb7ee8cb64102146e@ec2-54-235-246-201.compute-1.amazonaws.com:5432/ddqto13jodnbe6?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory")
+
+
+app.use(cors());
+
+app.get('/', (req, res) => res.send('{"name":"bryan"}'))
+
+//endpoint test
+app.listen(port, () => console.log(`ToGo app listening on port ${port}!`))
+
+
+//get user object by e-mail 
+app.get('/user/:email', (request, response) => { 
+    database.one(`SELECT * FROM "user" WHERE userEmail = '${request.params.email}'`)
+    .then((data) => {
+        response.json(data);
+    })
+    .catch((error) => {
+        response.send(error);
+    }) 
+
+});
