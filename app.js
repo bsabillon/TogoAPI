af9857910c4c89eb12a3863e62ea0981d2aa5ea4
bsabillon <38368514+bsabillon@users.noreply.github.com>
2019-08-08 20:08:09 -0600

    added local database to test

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cors = require('cors');
 var pgp = require("pg-promise")();
 var database = pgp("postgres://ofsoasjazfznro:8422453c66450cd02fe7a41a3fa1d20882e038ab1345b76bb7ee8cb64102146e@ec2-54-235-246-201.compute-1.amazonaws.com:5432/ddqto13jodnbe6?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory")
 
+//var database = pgp("postgres://postgres:gaballu@localhost:5432/postgres")
 app.use(cors());
 
 //endpoint test
@@ -17,7 +18,7 @@ app.get('/user/:email', (request, response) => {
         response.json(data);
     })
     .catch((error) => {
-        response.send(error);
+        response.send("ERROR" + error);
     }) 
 
 });
