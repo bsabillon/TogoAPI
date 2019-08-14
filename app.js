46789c3083371af2e05276237ad660c119d11e49
bsabillon <38368514+bsabillon@users.noreply.github.com>
2019-08-14 00:33:06 -0600

    added get methods

--- app.js
+++ app.js
@@ -5,15 +5,57 @@ var cors = require('cors');
 var pgp = require("pg-promise")();
 var database = pgp("postgres://ofsoasjazfznro:8422453c66450cd02fe7a41a3fa1d20882e038ab1345b76bb7ee8cb64102146e@ec2-54-235-246-201.compute-1.amazonaws.com:5432/ddqto13jodnbe6?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory")
 
-//var database = pgp("postgres://postgres:gaballu@localhost:5432/postgres")
+
 app.use(cors());
 
 //endpoint test
 app.get('/', (req, res) => res.send('{"status":"correct"}'))
 
+
+//create new user
+app.post('/user', (request, response) => { 
+            
+    database.query('INSERT INTO "user" (${this:name}) VALUES (${this:csv})',
+    request.body)
+    .then((data) => {
+        response
+        .status(200)
+        .json('{"response" : "user added succesfully!"}');
+    })
+    .catch( (error) => {
+        response.send(error);
+    });
+
+});
+
 //get user object by e-mail 
 app.get('/user/:email', (request, response) =>  {
     database.one(`SELECT * FROM "user" WHERE "userEmail" = '${request.params.email}'`)
+    .then((data) => {
+        response.json(data);
+    })
+    .catch((error) => {
+        response.send("ERROR" + error);
+    }) 
+
+});
+
+//get all user object by e-mail 
+app.get('/user/', (request, response) =>  {
+    database.any(`SELECT * FROM "user" `)
+    .then((data) => {
+        response.json(data);
+    })
+    .catch((error) => {
+        response.send("ERROR" + error);
+    }) 
+
+});
+
+
+//get all store object by sellerId 
+app.get('/store/', (request, response) =>  {
+    database.any(`SELECT * FROM "store" `)
     .then((data) => {
         response.json(data);
     })
@@ -34,5 +76,31 @@ app.get('/store/:sellerId', (request, response) =>  {
     }) 
 
 });
+
+//get product object by id
+app.get('/product/:productId', (request, response) =>  {
+    database.one(`SELECT * FROM "product" WHERE "productId" = '${request.params.productId}'`)
+    .then((data) => {
+        response.json(data);
+    })
+    .catch((error) => {
+        response.send("ERROR" + error);
+    }) 
+
+});
+
+//get all product objects 
+app.get('/product/', (request, response) =>  {
+    database.any(`SELECT * FROM "product" `)
+    .then((data) => {
+        response.json(data);
+    })
+    .catch((error) => {
+        response.send("ERROR" + error);
+    }) 
+
+});
+
+
 
 app.listen(process.env.PORT || port, () => console.log(`ToGo app listening on port ${port}!`))
